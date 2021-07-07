c9a44718f99ab6f109f6c017a8a5e7d6092e0284
Sebastian Spaink <sspaink@influxdata.com>
2021-07-07 16:55:20 -0500

    Get outputs working with execd
    Co-authored-by: Mya Longmire <MyaLongmire@users.noreply.github.com>

--- config/config.go
+++ config/config.go
@@ -1097,6 +1097,9 @@ func (c *Config) addOutput(name string, table *ast.Table) error {
 		return err
 	}
 
+	// Use line of the plugin table as ID
+	outputConfig.ID = table.Position.Begin
+
 	if err := c.toml.UnmarshalTable(table, output); err != nil {
 		return err
 	}
@@ -1147,7 +1150,7 @@ func (c *Config) addInput(name string, table *ast.Table) error {
 	}
 
 	// Use line of the plugin table as ID
-	pluginConfig.ID = table.Line
+	pluginConfig.ID = table.Position.Begin
 
 	if err := c.toml.UnmarshalTable(table, input); err != nil {
 		return err
